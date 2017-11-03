f93823ddf212750561cc2bc1bec400e0a5b0b475
Firas Chaaben <firaschaabencss@gmail.com>
2017-11-03 10:50:28 +0100

    first file init

---
+++ generator.py
@@ -0,0 +1,32 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class Handler:
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+    def getpass(self, butgenerator):
+        print("generator password")
+        
+    def savepass(self, butsave):
+        print("save password")
+        
+    def resetpass(self, butreset):
+        print("reset password")
+            
+
+
+builder = Gtk.Builder()
+builder.add_from_file("generator-layout.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("window1")
+window.connect("delete-event", Gtk.main_quit)
+window.set_default_size(400, 300)
+window.show_all()
+
+
+Gtk.main()
+
